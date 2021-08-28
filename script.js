b114c6247d58d888e44ba980920394c189fac486
saqibmohammad <imcsaquib@gmail.com>
2021-08-28 10:27:03 +0530

    final

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const car =['ford','fiat','ferrari']
+const [one,two,three=car;
+console.log(one)
+
+const num =[1,2,3,4]
+const num2=[6,7,8,9]
+
+const combinenum =[...num,...num2]
+console.log(combinenum)
+
+const pname ={name:'saqib'}
+const ppost ={job:'Student'}
+const combinep={...first,place:'Delhi',...second}
+console.log(combinep)
